3ecdbd9f61223df3d77c00f2fbcdc0a003250e3c
Ella Berg <ellamadelineberg@gmail.com>
2020-11-15 14:34:40 +0200

    Refactor Execute: Provide more concise documentation and rename the package

---
+++ commonjs_patterns/homemade_module_loader/execute/execute.js
@@ -0,0 +1,16 @@
+// fs-module is needed in order to access the underlying operating system and its file system
+const fs = require('fs')
+
+/*
+  Creates a function expression by manually wrapping the file's code alongside
+  with the declarations into a template string.
+*/
+function execute(filename) {
+  const wrapperSrc =
+    `(function() {
+      ${fs.readFileSync(filename, 'utf8')}
+    })()`
+  eval(wrapperSrc)
+}
+
+execute('./print_quote.js')
--- commonjs_patterns/homemade_module_loader/execute/print_quote.js
+++ commonjs_patterns/homemade_module_loader/execute/print_quote.js
@@ -1,3 +1,3 @@
-/* A simple program to print my favourite film quote. */
+/* A simple program to print a quote from the film Piano. */
 const piano_quote = `Down there everything is so still and silent that it lulls me to sleep.`
 console.log(piano_quote)
