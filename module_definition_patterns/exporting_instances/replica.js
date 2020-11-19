a168cd6c64f1fc032419fce751e2174db850d769
Ella Berg <ellamadelineberg@gmail.com>
2020-11-19 21:53:03 +0200

    Refactor replica.js: Rename copy() to replicate() and getCount() to getCopyCount() Supply more verbose comments & documentation

--- module_definition_patterns/exporting_instances/replica.js
+++ module_definition_patterns/exporting_instances/replica.js
@@ -1,28 +1,35 @@
+/** Do Androids Dream of Electric Sheep? **/
+class Replica {
+  constructor (original) {
+    this.original = original
+    this.copyCount = 0
+  }
+
+  /*
+    The difference between copying and replicating is that replication imitates
+    the original process (e.g. creates a copy of the original painting by using
+    the same methods).
+
+    Here, we are building a new Android by by using the same assembly line.
+  */
+  replicate() {
+    this.copyCount++
+    console.log(`
+      New copy of ${this.original} produced succesfully.
+      Current count: ${this.copyCount}
+      `)
+  }
+
+  getCopyCount() {
+    console.log(this.copyCount)
+  }
+}
 /**
-  The instance will be cached and each file that requires the Replica module
-  will refer to the cached module first. However, please note that there is no
+  The instance of the class Replica will be cached and each constant that requires
+  the module will refer to the cached module first. Please note that there is no
   guarantee that the cache will not change in the future.
 
   If the cache has been reset the module will be re-initialized and the state
   will be lost.
 **/
-class Replica {
-  constructor (original) {
-    this.count = 0
-    this.original = original
-  }
-
-  copy() {
-    this.count++
-    console.log(`
-      New copy of ${this.original} produced succesfully.
-      Current count: ${this.count}
-      `)
-  }
-
-  getCount() {
-    console.log(this.count)
-  }
-}
-
 module.exports = new Replica('Rachael') // Exporting an instance instead of a whole class
