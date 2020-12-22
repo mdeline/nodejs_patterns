2a45b92240fbd46fea4551b056302dab9c5c2059
Ella Berg <ellamadelineberg@gmail.com>
2020-12-22 13:50:10 +0200

    Showcase default exports, named exports, namespace imports and mixed imports

--- module_systems/esm_patterns/default_exports/index.js
+++ module_systems/esm_patterns/default_exports/index.js
@@ -1,4 +1,16 @@
-// Default exports -malli mahdollistaa moduulin uudelleennimeämisen halutunlaiseksi heti tuonnin yhteydessä
-import DorothysLine from './silver_shoes.js' 
+// 1. Default exports
+import DorothysLine from './oz.js' // importin uudelleennimeäminen halutunlaiseksi heti tuonnin yhteydessä ilman 'as' avainsanan käyttöä
 DorothysLine()
 
+// 2. Named exports
+import { scarecrow as scarecrowsline } from './oz.js'
+scarecrowsline()
+
+// 3. Namespace import
+import * as ozmodule from './oz.js'
+ozmodule.cowardly_lion()
+
+// 4. Mixed imports
+import Dorothy, { tin_man } from './oz.js'
+Dorothy()
+tin_man()
