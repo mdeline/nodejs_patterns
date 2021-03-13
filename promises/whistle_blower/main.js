c07a1e254c2b140b804d97dbe231f8f13246b932
Ella Berg <ellamadelineberg@gmail.com>
2021-03-13 16:30:15 +0200

    Add another example of using promises to whistle blower

--- promises/whistle_blower/main.js
+++ promises/whistle_blower/main.js
@@ -1,5 +1,10 @@
-(new Promise((resolve, reject) =>  {})) // fullfills or rejets based on the function given to the promise
+import { evaluate, re } from 'mathjs'
 
+// Example of creating a new promise
+(new Promise((resolve, reject) =>  {})) // Promise fullfills or rejects based on the function given to the promise
+
+
+// example 1.
 function whistle_after(milliseconds) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -11,4 +16,16 @@ function whistle_after(milliseconds) {
 whistle_after(2000)
     .then(sound => {
         console.log(sound)
+    })
+
+// example 2.
+function cast(a_spell) {
+    return new Promise((resolve, reject) => {
+        resolve(evaluate(a_spell))
+    })
+}
+
+cast(18000 * 1.07 + 18000 * 1.07 + 18000 * 1.07)
+    .then(result => {
+        console.log(result)
     })
