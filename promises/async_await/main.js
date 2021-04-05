b6ef45700184516cbb3e051b984da12e2c36770b
Ella Berg <ellamadelineberg@gmail.com>
2021-04-05 11:24:11 +0300

    Promisify setTimeout and name it to sleep

--- promises/async_await/main.js
+++ promises/async_await/main.js
@@ -1,28 +1,34 @@
-function oven_is_hot() {
+import { promisify } from 'util'
+
+/* 
+Async await ei toimi takaisinkutsujen kanssa, koska toiminnallisuus on rakennettu Promise-olioiden päälle syntaktiseksi helpotukseksi.
+Jos async awaitia halutaan käyttää callbackien kanssa, niin callback täytyy ensin muuttaa promiseksi promisify-funktion avulla.
+*/
+const sleep = promisify(setTimeout)
+
+async function oven_is_hot() {
+    await sleep(1000)
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(notify(`Oven is hot.`))
-        }, 1500)
+        resolve(notify("Oven is hot."))
     })
 }
 
-function dough_has_risen() {
+async function dough_has_risen() {
+    await sleep(1000)
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(notify(`Dough has risen.`))
-        }, 1500)
+        resolve(notify(`Dough has risen.`))
     })
 }
 
-function pizza_is_baked() {
+async function pizza_is_baked() {
+    await sleep(1000)
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(notify(`Your pizza is ready.`))
-        }, 1500)
+        resolve(notify(`Your pizza is ready.`))
     })
 }
 
-function eat() {
+async function eat() {
+    await sleep(1000)
     return new Promise((resolve, reject) => {
         resolve(notify('There’s very little in my world that a foot massage and a thin-crust pizza won’t set right.'))
     })
@@ -32,14 +38,11 @@ function notify(message) {
     console.log(message)
 }
 
-/* 
-Async await ei toimi takaisinkutsujen kanssa, koska toiminnallisuus on rakennettu Promise-olioiden päälle syntaktiseksi helpotukseksi.
-Jos async awaitia halutaan käyttää callbackien kanssa, niin callback täytyy ensin muuttaa promiseksi promisify-funktion avulla.
-*/
 async function cook_pizza() {
     await oven_is_hot()
     await dough_has_risen()
     await pizza_is_baked()
 }
 
+/* --- */
 cook_pizza().then(eat)
